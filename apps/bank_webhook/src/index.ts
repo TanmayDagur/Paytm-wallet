3fe2c15bfe936a20006b402bbd36f54bfb04ac91
Tanmay Dagur <82839812+TanmayDagur@users.noreply.github.com>
2025-08-21 18:29:36 +0530

    Update index.ts

--- apps/bank_webhook/src/index.ts
+++ apps/bank_webhook/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://paytm-style-wallet-user-app-hjnq-ashen.vercel.app",
+  origin: "https://paytm-nine-cyan.vercel.app/",
   credentials: true,
 }));
 
