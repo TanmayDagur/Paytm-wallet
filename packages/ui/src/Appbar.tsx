b1e63ba47bd55a87b0236376ed568e4cb74b9b15
Tanmay Dagur <82839812+TanmayDagur@users.noreply.github.com>
2025-08-21 19:53:21 +0530

    Update Appbar.tsx

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -4,7 +4,7 @@ interface AppbarProps {
     user?: {
         name?: string | null;
     },
-    onSignin: () => void ,
+    onSignin: () => void,
     onSignout: () => void
 }
 
@@ -13,12 +13,19 @@ export const Appbar = ({
     onSignin,
     onSignout
 }: AppbarProps) => {
-    return <div className="flex justify-between border-b px-4">
-        <div className="text-lg flex flex-col justify-center">
-            PayTM
+    return (
+        <div className="flex justify-between items-center border-b px-10 py-3 bg-white shadow-md">
+            <div className=" text-2xl font-extrabold bg-gradient-to-r from-cyan-500 to-blue-600 bg-clip-text text-transparent tracking-wide select-none cursor-pointer hover:scale-105 transition-transform">
+
+                PayTM
+
+            </div>
+
+            <div className="pt-1">
+                <Button onClick={user ? onSignout : onSignin}>
+                    {user ? "Logout" : "Login"}
+                </Button>
+            </div>
         </div>
-        <div className="flex flex-col justify-center pt-2">
-            <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
-        </div>
-    </div>
+    );
 }
